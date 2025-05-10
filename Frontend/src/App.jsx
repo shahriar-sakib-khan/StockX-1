8dabbb57f4ecafb857a5f6aedbb1f3c10e046429
Ebtehaj Ahmed <ahmedratul216@gmail.com>
2025-05-11 03:59:41 +0600

    Create the basic frontend file structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import styles from "./App.module.css";
+
+export default function App() {
+  return (
+    <div className={styles.app}>
+      <h1>This is the App Component</h1>
+    </div>
+  );
+}
