adcb71690ec5e37e4c3379d46ff51c414b6bd2a1
s-a-alif <soroarazmiralif@gmail.com>
2025-05-18 21:09:52 +0600

    Add controllers and middlewere at backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,27 @@
-const dotenv = require('dotenv').config()
-const mongoose = require('mongoose')
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const app = express();
 
-// Connecting to databse(Mongodb)
-mongoose.connect(process.env.MONGO_URL, console.log("Database connected successfully"))
+const authController = require('./controllers/authController')
+const uploadController = require('./controllers/uploadController');
+const userController = require("./controllers/userController");
+const commentController = require("./controllers/commentController");
 
-// Starting server
-app.listen(process.env.PORT, console.log("Server has been started successfully"))
+// db connecting
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URL);
 
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authController);
+app.use('/upload', uploadController)
+app.use('/user', userController)
+app.use('/comment', commentController)
+
+// starting server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Server has been started"));
