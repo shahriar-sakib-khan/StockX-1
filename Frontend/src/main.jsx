ffd30003f7ee669e19b3b2d20a4425d8773f7dd5
S-H-Jisan <shzisun123@gmail.com>
2025-05-31 23:32:54 +0600

    added login and register pages and removed index.css from main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./index.css";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,11 +1,108 @@
+import React , {useState} from 'react';
 import { NavLink } from "react-router-dom";
-import styles from "./Login.module.css";
+import styles from './Login.module.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import {toast} from "react-toastify";
 
-export default function Login() {
-  return (
-    <div className={styles.login}>
-      <h1>Login page</h1>
-      <NavLink to="/dashboard">Login</NavLink>
-    </div>
-  );
+const Login = () =>{
+    const [email , setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword , setShowPassword] = useState(false);
+
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        if(!email.trim()){
+            toast.error("Email is required", {
+                toastId: 0,
+            });
+            return;
+        }
+
+        if(!password.trim()){
+            toast.error("Password is required",{
+                toastId: 1,
+            });
+            return;
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            toast.error("Enter A Valid Email Address" ,{
+                toastId: 2,
+            });
+            return;
+        }
+
+        toast.success("Form Submitted Successfully" , {
+            toastId: 3,
+        });
+    }
+    return(
+        <>
+            <div className={styles.main_container}>
+                <div className={styles.form_container}>
+                    <div className={styles.header}>
+                        <h2>Welcome back!</h2>
+                    </div>
+                    <div className={styles.google_login}>
+
+                    </div>
+                    <div className={styles.orLine}>
+                        <div className={styles.Line}></div>
+                        <span>OR</span>
+                        <div className={styles.Line}></div>
+                    </div>
+
+                    <div className={styles.form}>
+                        <form >
+                            <div>
+                            <p className={styles.email}>Email</p>
+                            <i className={`fas fa-envelope ${styles.email_icon}`}></i>
+                            <input type = "email"
+                                   className = {styles.input_box}
+                                   value = {email}
+                                   placeholder = "Enter Your Email"
+                                   onChange = {e => setEmail(e.target.value)}
+                                   required
+                            >
+                            </input>
+                            </div>
+
+
+                            <div className = {styles.password_container}>
+                                <p>Password</p>
+
+                                <i className={`fas fa-lock ${styles.pass_icon}`}></i>
+                                <input type = {showPassword ? "text" : "password"}
+                                       value = {password}
+                                       placeholder = "Enter Your Password"
+                                       onChange = {e => setPassword(e.target.value)}
+                                       required
+                                />
+
+                                {/*<button type = "button" onClick={()=> setShowPassword(!showPassword)}>*/}
+                                {/*    {showPassword ? <FaEyeSlash /> : <FaEye />}*/}
+                                {/*</button>*/}
+                                <div className={styles.forget_pass}>
+                                    <a href="#forget_password">Forget Password?</a>
+                                </div>
+                            </div>
+                            <div className={styles.buttons}>
+                                <NavLink to="/dashboard" className={styles.nav}><button>Login</button></NavLink>
+                                <NavLink to="/register" className={styles.nav}><button>Register</button></NavLink>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className={styles.help_box}>
+                    <i className={`fas fa-question-circle ${styles.help_icon}`}></i>
+                    <p>Help</p>
+                </div>
+            </div>
+        </>
+    );
 }
+
+export default Login;
