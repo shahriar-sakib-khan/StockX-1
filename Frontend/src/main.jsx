16cb1ca945f43e04a02ac29c59aeb0db0af5047c
S-H-Jisan <shzisun123@gmail.com>
2025-06-04 16:52:10 +0600

    merged register

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./index.css";
 import App from "./App";
+import './index.css';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
