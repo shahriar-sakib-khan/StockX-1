cb1e319fc713e5a32fc15486683e64687efa7459
S-H-Jisan <shzisun123@gmail.com>
2025-06-04 17:15:14 +0600

    merged profile in main

---
+++ Frontend/src/pages/Profile/Employees.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Employees.module.css';
+import employees_pic from '../../assets/images/employee_pic.png';
+
+const Employees = ({data , onEdit , onDelete}) =>{
+    return (
+        <>
+            <div className = {styles.employee_box}>
+                <div className = {styles.img_box}>
+                    {data.picture && (
+                        <img src = {data.picture} alt = {employees_pic} className = {styles.employee_pic}/>
+                    )}
+                </div>
+                <div className ={styles.empInfo_box}>
+                    <p><strong>Name: </strong> {data.name}</p>
+                    <p><strong>Designation: </strong> {data.designation}</p>
+                    <p><strong>Phone:</strong>{data.phoneNumber}</p>
+                </div>
+                <div className = {styles.empButton_container}>
+                    <button onClick={onEdit} className={styles.edit_btn}>Edit</button>
+                    <button onClick={onDelete} className={styles.remove_btn}>Remove</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Employees;
