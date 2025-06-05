88c029ded8c3ed979e0873d50c742f88c3b58cdf
s-a-alif <soroarazmiralif@gmail.com>
2025-06-05 20:00:58 +0600

    Fix login page css

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,10 +1,41 @@
-import React  from 'react';
-import { NavLink } from "react-router-dom";
+import {useNavigate, NavLink } from "react-router-dom";
 import styles from './Login.module.css'
 import { FcGoogle } from 'react-icons/fc';
 import { FaUser, FaEnvelope, FaLock } from 'react-icons/fa';
 
+import { AuthContext } from "../../context/AuthContext";
+import { useState, useContext } from "react";
+
+
 const Login = () =>{
+    const [showPassword , setShowPassword] = useState(false);
+        const [inputs, setInputs] = useState({ email: "", password: "" });
+        const { login } = useContext(AuthContext);
+        const navigate = useNavigate();
+    
+        const handleChange = (e) => {
+        setInputs({ ...inputs, [e.target.name]: e.target.value });
+        };
+    
+        const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch("http://localhost:3000/api/v1/auth/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(inputs),
+            });
+            const data = await res.json();
+            if (res.ok) {
+                login(data.token);
+                navigate("/dashboard");
+            } else {
+                alert(data.error);
+            }
+        } catch (err) {
+            alert("Login failed");
+        }
+    };
 
     return(
         <>
@@ -28,17 +59,27 @@ const Login = () =>{
                         <div className={styles.inputBox}>
                             <p>Email</p>
                             <FaEnvelope className={styles.icon} />
-                            <input type="email" placeholder="example@site.com" />
+                            <input 
+                            name="email" 
+                            type = "email"
+                            onChange={handleChange} 
+                            placeholder="example@site.com"
+                            required/>
                         </div>
 
                         <div className={styles.inputBox}>
                             <p>Password</p>
                             <FaLock className={styles.icon} />
-                            <input type="password" placeholder="Minimum 8 characters" />
+                            <input 
+                            name="password"
+                            type = {showPassword ? "text" : "password"}
+                            onChange={handleChange} 
+                            placeholder="Minimum 8 characters" 
+                            required />
                         </div>
 
                         <div className={styles.button_container}>
-                            <NavLink to="/dashboard"><button type="submit"> Login</button></NavLink>
+                            <NavLink to="/dashboard"><button onClick={handleLogin}> Login</button></NavLink>
                         </div>
 
                     </form>
